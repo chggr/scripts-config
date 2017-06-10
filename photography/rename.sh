cd653781e84ce81d1be3bf168236408d7a72653d
Christos Gkekas <chris.gekas@gmail.com>
2017-06-10 17:35:55 +0100

    Adding script rename.sh under directory 'photography'

---
+++ photography/rename.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script renames all JPG photographs and associated files (RAW, XMP, etc)
+# in the current directory. It uses a simple naming scheme, i.e. 00001.jpg,
+# 00002.jpg, etc which can easily be changed to something more elaborate.
+#
+# Cameras typically store images using the DSC****.jpg pattern and the numbers
+# are rarely continuous as some images might have been deleted in camera or
+# during post processing. Running this script ensures that images and related
+# files follow the same consistent naming pattern before they can be archived.
+
+counter=1
+
+for jpgFile in $(ls *.JPG *.jpg)
+do
+  # Extract filename.
+  jpgFileName=${jpgFile%.*}
+
+  # Rename all files using the counter.
+  for file in $(ls $jpgFileName.*)
+  do
+    mv $file `printf "%05d" $counter`.${file#*.}
+  done
+
+  # Increment the counter.
+  let "counter += 1"
+done
+
