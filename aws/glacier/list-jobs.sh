34816bb6301b1ea01b3025f362b7bae365f8ea60
Christos Gkekas <chris.gekas@gmail.com>
2017-10-21 11:13:53 +0100

    Adding AWS glacier script list-jobs.sh

---
+++ aws/glacier/list-jobs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is used to list all jobs that have been submitted against a
+# given vault. It returns information such as job type, submission time and
+# current status.
+
+if [ "$#" -ne 2 ]
+then
+    echo "Usage $0 <VAULT>"
+    exit 1
+fi
+
+VAULT=$1
+
+aws glacier list-jobs --account-id - --vault-name "$VAULT"
+
+exit 0
+
