cb40fc8c40c21ef2693c277e3306b53feefc7705
Christos Gkekas <chris.gekas@gmail.com>
2017-10-21 11:33:57 +0100

    Remove REGION from inventory-retrieval.sh

--- aws/glacier/inventory-retrieval.sh
+++ aws/glacier/inventory-retrieval.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # This script is used to submit a job to AWS Glacier to retrieve the inventory
-# of a given vault in the specified region. It returns the id of the job
-# created and its location. The current status of the job and the final output
-# can be obtained via other scripts.
+# of a given vault. It returns the id of the job created and its location. The
+# job will typically take multiple hours to complete. Its current status and
+# the final output can be obtained via other scripts.
 
-if [ "$#" -ne 2 ]
+if [ "$#" -ne 1 ]
 then
-    echo "Usage $0 <VAULT> <REGION>"
+    echo "Usage $0 <VAULT>"
     exit 1
 fi
 
 VAULT=$1
-REGION=$2
 
-aws glacier initiate-job --account-id - --region "$REGION" --vault-name "$VAULT" \
+aws glacier initiate-job --account-id - --vault-name "$VAULT" \
                          --job-parameters "{\"Type\": \"inventory-retrieval\"}"
 
 exit 0
